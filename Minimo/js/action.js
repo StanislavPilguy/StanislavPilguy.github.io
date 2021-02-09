93248616dcc349a35d4374f5336877b3b185fb38
StanislavPilguy <stanislav.pilguy@gmail.com>
2021-02-09 12:15:21 +0200

    add minimo

---
+++ Minimo/js/action.js
@@ -0,0 +1,7 @@
+var btn = document.getElementById('bnt');
+btn.onclick = function () {
+
+    document.querySelector('.li').classList.toggle('mobMenu');
+    btn.classList.toggle('changeImage');
+
+};
